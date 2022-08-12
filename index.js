fb1ea844a2c8813766b5e77e17502db323bb095c
zakariaahmad <kimo.ahmad2009@gmail.com>
2022-08-12 18:40:52 +0300

    Done

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const totalBatteries = batteryBatches.reduce(batteryCounter, 0);
+
+function batteryCounter(totalCount, location) {
+    return totalCount + location;
+}
